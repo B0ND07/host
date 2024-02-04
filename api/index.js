9cd20bc40a63c5c023d933430adac0db18d3d287
b0nd07 <mhashimkp@gmail.com>
2024-02-04 10:55:28 +0530

    Revert "check lh"
    This reverts commit 9ef146e6110627f6310c3f495534f0bcf4cb4951.

--- api/index.js
+++ api/index.js
@@ -14,16 +14,9 @@ const env = require("dotenv").config();
 connect();
 app.use(cookieParser());
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["https://napbook.vercel.app"],
-//     credentials: true,
-//   })
-// );
-
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://napbook.vercel.app"],
     credentials: true,
   })
 );
